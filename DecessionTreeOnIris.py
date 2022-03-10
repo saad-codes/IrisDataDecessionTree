bad03016cc31abd384f904fbc0d9e932798cec49
saad-codes <muhammadsaadmath@gmail.com>
2022-03-10 10:26:38 +0500

    Random state updation

--- DecessionTreeOnIris.py
+++ DecessionTreeOnIris.py
@@ -1,5 +1,4 @@
 # Importing libraries
-from turtle import title
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 from sklearn.tree import plot_tree
@@ -19,7 +18,6 @@ fig = plt.figure(figsize=(25,20))
 ax = sns.kdeplot(data = pd.DataFrame(iris.data, columns= iris.feature_names))
 plt.title("KDE Plot of features")
 plt.xlabel("Values in cm")
-
 plt.show()
 # test train split
 X_train, X_test, y_train, y_test = train_test_split(X,y,random_state = 50, test_size = 0.24)
@@ -35,7 +33,7 @@ params = {
 treeClass = DecisionTreeClassifier(random_state=50)
 grid_search = GridSearchCV(estimator=treeClass, 
                            param_grid=params, 
-                           cv=7, n_jobs=1,verbose=1,  scoring = "accuracy")
+                           cv=7, n_jobs=-1,verbose=1,  scoring = "accuracy")
 # Training Data 
 grid_search.fit(X_train, y_train)
 # training Accuracy
@@ -43,7 +41,7 @@ score_df = pd.DataFrame(grid_search.cv_results_)
 # getting the estimator that gives best tarining results
 dt_best = grid_search.best_estimator_
 print("The Accuracy of train Data",dt_best.score(X_train,y_train))
-print("Best Classification tree accoring to Gradient Decent is \n",dt_best)
+print("Best Classification tree accoring to Gradient Search is \n",dt_best)
 # Printing Graphing tree
 fig = plt.figure(figsize=(25,20))
 _ = plot_tree(dt_best,feature_names=iris.feature_names, class_names=iris.target_names,filled=True)
